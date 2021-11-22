43c6a3cd4cfd009852285c0faea585ef922d98f3
judy-n <judynaamani@icloud.com>
2021-11-22 00:54:16 -0500

    add time parameter

--- examples.js
+++ examples.js
@@ -1,3 +1,3 @@
 "use strict";
 
-const todo = new TodoTracker()
+const todo = new TodoTracker(3)
--- todotracker.js
+++ todotracker.js
@@ -1,7 +1,46 @@
 "use strict";
 
-function TodoTracker() {
-    
+function TodoTracker(time) {
+
+    // Number of months for the graph
+    this.length = time;
+
+    // If time parameter wasn't passed in, default is 122 days (4 months)
+    if (time == undefined) {
+        this.length = 4;
+    }
+
+    // let num_months = Math.floor(this.length / 30); 
+
+    // The starting month will be the current month
+    const d = new Date();
+    let curr_month = d.getMonth();
+    let curr_year = d.getFullYear();
+
+    const month_names = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", 
+                    "Sep", "Oct", "Nov", "Dec"]
+    let month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+    // Check if it's a leap year
+    if(curr_year % 4 == 0){
+        month_days[1] = 29;
+    }
+
+    let total_days = 0;
+    let counter = 0;
+    let j = curr_month;
+    let months_to_display = [];
+    while (counter < this.length) {
+        if (j > 11) {
+            j = 0
+        }
+        months_to_display.push(month_names[j])
+        total_days += month_days[j]
+        counter ++;
+        j++;
+    }
+    // console.log(months_to_display)
+
     this.todolist = []
     
     this.element = document.createElement('div')
@@ -21,10 +60,6 @@ function TodoTracker() {
 
     <div class="graph">
     <ul class="months">
-      <li>Sep</li>
-      <li>Oct</li>
-      <li>Nov</li>
-      <li>Dec</li> 
     </ul>
     <ul class="days">
       <li>Sun</li>
@@ -40,13 +75,22 @@ function TodoTracker() {
     </div>
     `
 
+    // Add months to DOM
+    const months = this.element.querySelector('.months');
+    for (let k = 0; k < months_to_display.length; k++){
+        months.insertAdjacentHTML('beforeend', `<li>${months_to_display[k]}</li>`)
+    }
+    months.style.gridTemplateColumns = "";
+
+
+
     // Add squares to DOM 
     const squares = this.element.querySelector('.squares');
-    for (var i = 1; i < 122; i++) {
+    for (let i = 1; i <= total_days; i++) {
     const level = Math.floor(Math.random() * 4);  
     squares.insertAdjacentHTML('beforeend', `<li data-level="${level}"></li>`);
     }
-
+    
     // Add button adds an element to the list
     this.element.querySelector('#add-btn').addEventListener('click', ()=>this.addToList(this.element))
 
